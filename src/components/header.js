034293fadd9f8def9d65cc4346cbedd356f98bdf
nitudeka <nitumonideka327@gmail.com>
2020-04-21 19:28:51 +0530

    added basic header and navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,30 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import "./header.scss"
+import HeaderBg from "../images/headerbg.jpg"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+const Header = ({ data }) => {
+  return (
+    <header className="header position-relative center">
+      <img
+        src={HeaderBg}
+        alt="header"
+        className="position-absolute h-100 w-100"
+      />
+      <div className="container">
+        <h1 className="header__heading">
+          <span className="header__heading--main display-1 d-block">
+            Nitu Deka
+          </span>
+          <span className="header__heading--sub display-4 d-block">
+            Full Stack Developer
+          </span>
+        </h1>
+      </div>
+      {/* <Img fixed={data.file.childImageSharp.fixed} /> */}
+    </header>
+  )
 }
 
 export default Header
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -28,12 +28,12 @@ NavbarLink.defaultProps = {
 
 const Navbar = () => {
   return (
-    <nav className="navbar py-4 center">
+    <nav className="navbar py-4 center w-100">
       <ul className="navbar__nav m-0 container d-flex justify-content-center">
-        <NavbarLink name="01 : Home" />
-        <NavbarLink name="02 : Works" />
-        <NavbarLink name="03 : About me" />
-        <NavbarLink name="04 : Contact" />
+        <NavbarLink to="/" name="01 : Home" />
+        <NavbarLink to="/works" name="02 : Works" />
+        <NavbarLink to="/about" name="03 : About me" />
+        <NavbarLink to="/contact" name="04 : Contact" />
       </ul>
     </nav>
   )
