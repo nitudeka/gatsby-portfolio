3c3b8f15884683919b8d63a2cd7aa282ff2fee2b
Nitu Deka <nitudeka@Developers-MacBook-Air.local>
2020-06-07 22:19:56 +0530

    changed header alignment

--- src/components/header.js
+++ src/components/header.js
@@ -25,12 +25,14 @@ const Header = () => {
       />
       <div className="container">
         <h1 className="header__heading">
-          <span className="header__heading--main display-1 d-block">
-            Nitu Deka
-          </span>
-          <span className="header__heading--sub display-4 d-block">
-            Full Stack Developer
-          </span>
+          <div>
+            <span className="header__heading--main display-1 d-block">
+              Nitu Deka
+            </span>
+            <span className="header__heading--sub display-4 d-block">
+              Full Stack Developer
+            </span>
+          </div>
         </h1>
       </div>
       {/* <Img fixed={data.file.childImageSharp.fixed} /> */}
