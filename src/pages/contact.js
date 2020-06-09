90d787a6bdac7ef2dfd32aa7a2307aac043ca6e6
Nitu Deka <nitudeka@Developers-MacBook-Air.local>
2020-06-09 21:58:31 +0530

    added state to contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,18 +1,50 @@
-import React from "react"
+import React, { useState } from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import "./contact.scss"
 
+const validateEmail = email => {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return re.test(email)
+}
+
 const Form = () => {
+  const [email, setEmail] = useState("")
+  const [subject, setSubject] = useState("")
+  const [message, setMessage] = useState("")
+
+  const submit = () => {
+    const isEmailValid = validateEmail(email)
+    if (!isEmailValid || !subject || !message) return
+    console.log(email, subject, message)
+  }
+
   return (
     <div className="contact__form">
       <div>
-        <input type="email" placeholder="Email" className="w-100" />
-        <input type="text" placeholder="Subject" className="w-100" />
-        <textarea rows="4" placeholder="Message" className="w-100"></textarea>
+        <input
+          onChange={e => setEmail(e.target.value)}
+          type="email"
+          placeholder="Email"
+          className="w-100 bg-danger"
+        />
+        <input
+          onChange={e => setSubject(e.target.value)}
+          type="text"
+          placeholder="Subject"
+          className="w-100 bg-danger"
+        />
+        <textarea
+          onChange={e => setMessage(e.target.value)}
+          rows="4"
+          placeholder="Message"
+          className="w-100 bg-danger"
+        ></textarea>
       </div>
-      <button className="contact__btn px-3 mt-4">Send</button>
+      <button onClick={submit} className="contact__btn px-3 mt-4">
+        Send
+      </button>
     </div>
   )
 }
